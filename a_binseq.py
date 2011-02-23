a02aff40e285a21eab1e8a7b05cbd85edbca649e
Brian Chen <eipxen@gmail.com>
2011-02-23 02:59:54 -0600

    Added a_choose_seq to the module ; performs the same function as strict_a_binseq but returns a list of indices instead of a list of {0,1} representing whether that index should be included or not.  This should be better time and space wise than very sparse lists

--- a_binseq.py
+++ a_binseq.py
@@ -27,3 +27,20 @@ def strict_a_binseq(num_on, seq_size):
 '''
 	do this recursively; make a "choose" method that returns a vector of 1s or 0s
 '''
+
+'''
+    a_choose_seq: generates the sequences as a list of indices instead of a
+    list of ones or zeros; this is a little more efficient, and using this
+    sort of list to do a bitmask-style operation is more efficient than 
+    doing a dot product of what is probably a very sparse list instead
+'''
+
+def a_choose_seq(num_on, seq_size):
+    if num_on == 0:
+        yield []
+    else:
+        for i in xrange(seq_size-num_on+1):
+            recur = a_choose_seq(num_on-1, seq_size-(i+1))
+            for x in recur:
+                yield [i] + [j+(i+1) for j in x]
+            
